539b6492a1ff2552b947d1c06180b6173601fa38
perita23 <hugobedmar10@gmail.com>
2024-11-14 17:47:35 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+header("Content-Type: application/json");
+
+// Verificar el método de la solicitud
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method == 'POST') {
+    $movementType = $_POST["movementType"];
+    $amount = $_POST["amount"];
+
+    if ($movementType == "DEPOSIT_TRANSACTION") {
+        switch ($amount) {
+            case 0:
+                # code...
+                break;
+            case 0:
+                # code...
+                break;
+            case 0:
+                # code...
+                break;
+            case 0:
+                # code...
+                break;
+
+            default:
+                # code...
+                break;
+        }
+    } else if ($movementType == "WITHDRAWAL_TRANSACTION") {
+        switch ($amount) {
+            case 0:
+                # code...
+                break;
+            case 0:
+                # code...
+                break;
+            case 0:
+                # code...
+                break;
+            case 0:
+                # code...
+                break;
+
+            default:
+                # code...
+                break;
+        }
+        $response = ["error" => "Movimiento no especificado"];
+    }
+
+} else {
+    http_response_code(405);
+    $response = ["error" => "Método no permitido. Use POST."];
+}
+
+echo json_encode($response);
+?>
